9b84992660f6517c7cbf214816cd2bff00b6e4e9
King Kong <king-kong@gmail.com>
2023-01-09 12:11:43 -0800

    animal passes props from data

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,14 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal({ animal }) {
+  return (
+    <>
+      <div className="animals" style={{ top: animal.top, left: animal.left }}>
+        <p className="animalName">{animal.name}</p>
+        {animal.type}
+        {animal.says}
+        <img src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
+      </div>
+    </>
+  );
 }
